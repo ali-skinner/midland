2d7396220a665f75dd1c2e862e6dafd5ca941778
ali-skinner <ali.skinner.15@gmail.com>
2024-12-01 13:09:55 -0600

    redacted array hmwrk completed

--- src/js-wk1/04_function_hmrk/index.js
+++ src/js-wk1/04_function_hmrk/index.js
@@ -1,7 +1,7 @@
 // ! 1. Pass a number into a function that will return the absolute value of that number
 // ! (absolute value of -1 is 1, and absolute value of 1 is 1) without the use of built in math functions.
 
-function absoluteNum(d){
+function absoluteNum(d) {
     if (d < 0) {
         return d * -1;
     }
@@ -14,21 +14,36 @@ console.log(absoluteNum(100000000));
 
 //! 2. Make a function called pow that accepts arguments x and y and returns the value of x to the y power
 
-function pow(x,y) {
-    return x**y;
+function pow(x, y) {
+    return x ** y;
 }
-console.log(pow(18,15));
+console.log(pow(18, 15));
 
 
 //! 3. Write a function that accepts an array of banned words and an array of words.
 //! If any of the banned words appear in the array of words, replace them with "REDACTED.
 
-function redactBadwords(approvedWords, bannedWords){
+// how does SPLICE behave in thiss function vs SLICE? SLICE creates a copy / SPLICE alters orig
 
+
+function redactBadWords(wordsToCheck, bannedWords) {
+
+    let answer = wordsToCheck.slice();
+        
+        for (let i=0; i < answer.length; i++) {
+           
+            for (let b=0; b < bannedWords.length; b++) {
+                
+                if (bannedWords[b]=== answer[i]) {
+                    answer[i]="REDACTED";
+            }
+        }
+    }
+    return answer;
 }
 
-let goodWords = ["good", "great", "bad", "wonderful"];
+let wowWords = ["good", "great", "bad", "wonderful"];
 let badWords = ["bad", "gross"];
 
-let result = redactBadWords(goodWords, badWords);
+let result = redactBadWords(wowWords, badWords);
 console.log(result);
