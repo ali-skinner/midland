e523903fe42c6acec5295e89206236864bcb7be6
ali-skinner <ali.skinner.15@gmail.com>
2024-12-06 14:00:41 -0600

    starting the simon gm

---
+++ src/js-wk2/Simon_project-wk2-d4/simon.js
@@ -0,0 +1,18 @@
+"use strict";
+
+// make a button that starts a Game
+const startButton ()
+
+// lights up its color for 1-2 seconds
+const lightUpColorButton() {
+    document.getElementsById("")
+}
+
+// document.getElementById("myBtn").addEventListener("click", displayDate);
+// document.getElementByTag("myBtn").addEventListener("click", displayDate);
+// <button onclick="myFunction()">display txt if i wanna</button> 
+
+
+
+array of numbs 0-3; funct to change color shed a timeout to change it and change back transtions color and track array, user click me aray, when numbs dont match, compare it, add subtract
+// if nums dont match, do this
