5df1c60209b7cd3ff74cda696a664110dd21a0aa
ali-skinner <ali.skinner.15@gmail.com>
2024-12-06 15:17:01 -0600

    fancy start button ooooo lala ha

--- src/js-wk2/Simon_project-wk2-d4/simon.js
+++ src/js-wk2/Simon_project-wk2-d4/simon.js
@@ -1,18 +1,25 @@
 "use strict";
 
 // make a button that starts a Game
-const startButton ()
+const startButton ();
 
 // lights up its color for 1-2 seconds
-const lightUpColorButton() {
-    document.getElementsById("")
-}
-
+const lightUpColorButton() => {
+    document.getElementsByClassName("gameButton");
+};
 // document.getElementById("myBtn").addEventListener("click", displayDate);
 // document.getElementByTag("myBtn").addEventListener("click", displayDate);
 // <button onclick="myFunction()">display txt if i wanna</button> 
 
 
+// Pattern for setInterval (does something repeatedly after the delay time)
+let intervalRef = setInterval(() => {
+    // What to do repeatedly after the interval time
+  }, 1000); // Time in miliseconds
 
-array of numbs 0-3; funct to change color shed a timeout to change it and change back transtions color and track array, user click me aray, when numbs dont match, compare it, add subtract
+
+
+
+
+// array of numbs 0 - 3; funct to change color shed a timeout to change it and change back transtions color and track array, user click me aray, when numbs dont match, compare it, add subtract
 // if nums dont match, do this
