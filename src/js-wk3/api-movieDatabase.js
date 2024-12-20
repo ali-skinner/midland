5e89d2366c83ad260ab305650783c062e8d2927b
ali-skinner <ali.skinner.15@gmail.com>
2024-12-20 17:28:43 -0600

    why isnt this easier. what am i missing ugh api.

--- src/js-wk3/api-movieDatabase.js
+++ src/js-wk3/api-movieDatabase.js
@@ -1,8 +1,58 @@
 "use strict";
 
-// send data requests to http://www.omdbapi.com/?t=star+wars&apikey=c52d83f7
+// send data requests to http://www.omdbapi.com/?s=${movieSearchResults}&apikey=c52d83f7
 // Poster API requests http://img.omdbapi.com/?apikey=c52d83f7&t=
 // apikey=c52d83f7
+
+document.getElementById("submit").addEventListener("click", searchResults());
+
+function searchResults () {
+    fetch('http://www.omdbapi.com/?s=${movieSearchResults}&apikey=c52d83f7')
+    // This returns a promise - an object that represents the eventual completion (or failure) of an operation
+    .then((apiResponse) => { // This handles the promise from the fetch request
+        if (!apiResponse.ok) { // If the promise fails... (usually a 4xx or 5xx status code)
+        // Do something if the promise does not succeed
+        const errorMessage = "500 Internal Server Error."
+        console.log(errorMessage);
+        }
+        // Processes the successful response (usually a 2xx status code)
+        if (apiResponse.ok) { 
+            const successMessage = "202 Accepted"
+            console.log(successMessage);
+            }
+        return apiResponse.json(); // This also returns a promise that resolves to the JSON object when parsing is complete
+    })
+    .then((data) => {
+        // handles the promise that was given by the return statement
+        const results = data.Title //trying to put the json object here and parse out applicable results to display on screen
+        const movieData = JSON.parse(results);
+        document.getElementsByClassName("resultsDivs").innerHTML = movieData.Title
+    });
+}
+
+//   document.getElementById("submit").addEventListener("click", () => {
+    // let userSearch = document.getElementById("search-box").value;
+    //     fetch(`http://www.omdbapi.com/?s=${userSearch}&apikey=c52d83f7`)
+    //       .then ((response1) => {
+    //         // display the poster img and movie info
+    //         console.log(response1);
+    //         if (!response1.ok) { 
+    //             // write an error message by alert?
+    //           console.log(response1.status);
+    //         }
+    //         return response1.json();
+    //       })
+    //       .then((res) => {
+
+    //         res.Search.forEach(element => {
+    //             console.log(element.Title)
+    //         });
+    //         // for each element of array returned from the DB, display it on screen;
+    //       console.log(res.Search)} );
+          
+    //   })
+// write a new fetch / then for the poster imgs ?
+
 
 
 // fetch(api.link) // This returns a promise - an object that represents the eventual completion (or failure) of an operation
@@ -34,45 +84,4 @@
     //         return response.json(); // This parses the response as a JSON, which also returns a promise
     //       })
     //       .then((res) => console.log(res)); // handles the promise given by the return statement
-    //   });
-
-      
-
-      document.getElementById("submit").addEventListener("click", () => {
-    let userSearch = document.getElementById("search-box").value;
-        fetch(`http://www.omdbapi.com/?s=${userSearch}&apikey=c52d83f7`)
-          .then ((response1) => {
-            // display the poster img and movie info
-            console.log(response1);
-            if (!response1.ok) { 
-                // write an error message by alert?
-              console.log(response1.status);
-            }
-            return response1.json();
-          })
-          .then((res) => {
-
-            res.Search.forEach(element => {
-                console.log(element.Title)
-            });
-            // for each element of array returned from the DB, display it on screen;
-          console.log(res.Search)} );
-          
-      })
-// write a new fetch / then for the poster imgs ?
-
-
-
-    //   // ------------------------------------------------------------------
-    //   document.getElementById("postF").addEventListener("click", () => {
-    //     let me = { first: "John", last: "Smith" };
-    //     let headers = new Headers(); // Creates a headers object
-    //     headers.append("Content-Type", "application/json"); // Tells the API that the request's content type is JSON
-    //     fetch("https://jsonplaceholder.typicode.com/posts/", {
-    //       method: "POST", // POST submits data to the server
-    //       body: JSON.stringify(me), // Turns the 'me' object into a JSON string to give to the server
-    //       headers: headers, // Sets the headers for the request
-    //     })
-    //       .then((response) => response.json()) // Returns a promise
-    //       .then((response) => console.log(response)); // Handles the promise
     //   });
