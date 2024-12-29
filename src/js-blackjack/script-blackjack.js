4ae65bd2f1c6ec66c4f93e681fdb8acae8e70878
ali-skinner <ali.skinner.15@gmail.com>
2024-12-29 16:18:28 -0600

    card value function started

--- src/js-blackjack/script-blackjack.js
+++ src/js-blackjack/script-blackjack.js
@@ -1,4 +1,18 @@
 "use strict";
 
-const suits = ["♥", "♦", "♣", "♠"];
-console.log(suits)
+const suit = ["♥", "♦", "♣", "♠"];
+console.log(suit)
+const value = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+console.log(value)
+
+
+function cardValue(card) {
+    if (isNaN(card)) {
+        if (card === "A") {
+            return 11;
+        } else {
+            return 10;
+        }
+    }
+    return parseInt(card);
+}
