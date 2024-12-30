56fa3cfa3c57b1b806370dc6dbc3a90e3ae8c13c
ali-skinner <ali.skinner.15@gmail.com>
2024-12-30 17:53:23 -0600

    added notes for next steps - working on drawCard funct

--- src/js-blackjack/script-blackjack.js
+++ src/js-blackjack/script-blackjack.js
@@ -1,85 +1,88 @@
 "use strict";
 
 const suits = ["♥", "♦", "♣", "♠"];
-console.log(suits)
 const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
-console.log(ranks)
 
-const playerSum = [];
-const dealerSum = [];
-const hiddenDealer = "";
+const playerSum = []; //should this catch numbers/ranks conv to #'s/ to be evaluated in the whoWon funct?
+const dealerSum = []; //should dealerHand / playerHand be added as step one to collect dealt cards (suit and rank), then playerSum used for ranks only?
+// break up the dealer/player hand arrays to build #'s only array? or is this irrelevant bc the cards are setup as objects and can use card.value or is that only good in the buildDeck funct?
 let fullDeck = [];
 
 buildDeck();
-shuffle(fullDeck);
+shuffleDeck(fullDeck);
+drawCard();
 
 
+//grab a card from the deck;
+function drawCard(deck) {
+    if (deck.length > 0) {
+        const randomDraw = (Math.floor(Math.random()) * deck.length);
+        const randomCard = deck.splice(randomDraw, 1);
+        return randomCard;
 
-
-
-
-function drawCard() {
-    let randomCard = (Math.floor(Math.random) * 52)
-    //    grab a card from the deck;
-}
-
-
-
-
-function calcPlayerSum() {
-
-
-}
-
-function calcDealerSum() {
-
-
-}
-
-
-
-function shuffle(deck) {
-    for (let i = deck.length - 1; i > 0; i--) {
-        const random = Math.floor(Math.random() * (i + 1));
-
-        [deck[i], deck[random]] = [deck[random], deck[i]]
+    } else {
+        buildDeck();
+        shuffleDeck();
     }
 }
 
-function buildDeck() {
-    fullDeck = []; //to reset deck each hand
-    suits.forEach((suit) => {
-        ranks.forEach((rank) => {
-            let card = {
-                suit: suit,
-                value: rank
-            };
-            fullDeck.push(card);
+
+
+
+    function calcPlayerSum() {
+
+
+    }
+
+    function calcDealerSum() {
+
+
+    }
+
+
+
+    function shuffleDeck(deck) {
+        for (let i = deck.length - 1; i > 0; i--) {
+            const random = Math.floor(Math.random() * (i + 1));
+
+            [deck[i], deck[random]] = [deck[random], deck[i]]
+        }
+    }
+
+    function buildDeck() {
+        fullDeck = []; //to reset deck each hand
+        suits.forEach((suit) => {
+            ranks.forEach((rank) => {
+                let card = {
+                    suit: suit,
+                    value: rank
+                };
+                fullDeck.push(card);
+            });
+
         });
+        console.log(fullDeck);
+    }
 
-    });
-    console.log(fullDeck);
-}
+    function getCardValue(card) {
+        if (isNaN(card.value)) {
+            if (card.value === "A") {
+                return 11;
+            } else {
+                return 10;
+            }
+        }
+        return parseInt(card.value);
+    }
 
-function getCardValue(card) {
-    if (isNaN(card.value)) {
+    function aceValue(card, playerSum) {
         if (card.value === "A") {
-            return 11;
-        } else {
-            return 10;
+            if (playerSum > 21) {
+                return 1;
+            } else {
+                return 11;
+            }
         }
+        return parseInt(card.value);
     }
-    return parseInt(card.value);
-}
-
-function aceValue(card, playerSum) {
-    if (card.value === "A") {
-        if (playerSum > 21) {
-            return 1;
-        } else {
-            return 11;
-        }
-    }
-    return parseInt(card.value);
-}
 
