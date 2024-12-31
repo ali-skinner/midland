6a46143d74e734283f564937e3b5810b821b4070
ali-skinner <ali.skinner.15@gmail.com>
2024-12-30 19:01:10 -0600

    v2  drawCard funct

--- src/js-blackjack/script-blackjack.js
+++ src/js-blackjack/script-blackjack.js
@@ -3,9 +3,11 @@
 const suits = ["♥", "♦", "♣", "♠"];
 const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
 
-const playerSum = []; //should this catch numbers/ranks conv to #'s/ to be evaluated in the whoWon funct?
-const dealerSum = []; //should dealerHand / playerHand be added as step one to collect dealt cards (suit and rank), then playerSum used for ranks only?
-// break up the dealer/playerHand arrays to make a #'s only array with playerSum? or is this irrelevant bc the cards are setup as objects and can use card.value or is that only good in the buildDeck funct?
+const playerHand = [];
+const dealerHand = [];
+const playerSum = [];
+const dealerSum = []; 
+
 let fullDeck = [];
 
 buildDeck();
@@ -14,7 +16,7 @@ drawCard(fullDeck);
 
 
 //grab a card from the deck;
-function drawCard(deck) {
+function drawCard(deck) {``
     if (deck.length > 0) {
         const randomDraw = Math.floor(Math.random() * deck.length);
         return deck.splice(randomDraw, 1) [0];
